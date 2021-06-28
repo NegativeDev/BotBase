d5cfba057de783710148846d212e267b81073e28
Negative <60104846+NegativeKB@users.noreply.github.com>
2021-06-28 16:29:29 -0230

    Removed nonused import

--- src/main/java/dev/negativekb/api/Bot.java
+++ src/main/java/dev/negativekb/api/Bot.java
@@ -7,7 +7,6 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
 
